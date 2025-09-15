aef4f10757843c8a77bc26fff629da0f96225be6
Roberto Kennedy <roberto.kpp@gmail.com>
2025-09-15 17:08:15 -0500

    ***

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let btn = document.getElementById("btn");
 let input = document.getElementById("add-item");
 
-let list = [];
+let list = [""];
 
 function addItemList() {
   list.push(input.value);
   console.log(list);
+  input.value = "";
 }
 
-btn.addEventListener("click", addItemList);
-
+btn.addEventListener("click", addItemList());
