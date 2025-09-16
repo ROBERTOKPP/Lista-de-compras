c183d77e091b067bda567cc7c3b2b49841db4cb2
Roberto Kennedy <roberto.kpp@gmail.com>
2025-09-16 11:28:12 -0500

    finalizado o js

--- script.js
+++ script.js
@@ -3,12 +3,25 @@ let input = document.getElementById("add-item");
 let fullList = document.querySelector(".list-task");
 let list = [];
 
+// adcionar o item na lista verificando se esta vazio
 function addItemList() {
-  list.push(input.value);
-  input.value = "";
-  newtasks();
+  const vlr = input.value.trim();
+  if (vlr == "") {
+    alert("Digite um valor valido!");
+    return;
+  } else {
+    list.push(input.value);
+    input.value = "";
+    newtasks();
+  }
 }
 
+// usar a tecla enter para acionar a função de adionar o item
+input.addEventListener("keydown", function (event) {
+  if (event.key == "Enter") addItemList();
+});
+
+// adcionar o item na tela - formatada ja
 function newtasks() {
   let newLi = "";
 
@@ -27,9 +40,10 @@ function newtasks() {
   fullList.innerHTML = newLi;
 }
 
+// deletar o item da lista e add o alert em baixo
 function deleteItem(position) {
   const checkbox = document.querySelector(`.check-${position}`);
-  const msAlert = document.getElementById("teste");
+  const msAlert = document.getElementById("alert-del");
   if (checkbox.checked) {
     list.splice(position, 1);
     itemAlert = `
@@ -38,11 +52,18 @@ function deleteItem(position) {
             <img src="assets/icons/warning-circle-filled.svg" alt="" />
             <span>O item foi removido da lista</span>
           </div>
-          <img src="assets/icons/delete-small.svg" alt="" />
+          <img src="assets/icons/delete-small.svg" alt="" onclick="delAlert()" />
         </div>
     `;
     msAlert.innerHTML = itemAlert;
     newtasks();
+  }
+}
+//deletar o alerta de exclusão do item
+function delAlert() {
+  const alertbox = document.getElementById("alert");
+  if (alertbox) {
+    alertbox.remove();
   }
 }
 
